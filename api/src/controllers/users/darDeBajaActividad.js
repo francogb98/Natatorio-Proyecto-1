9591545ef34db806a860d949d42a1dce3ef52110
francogb98 <francobaudino98@gmail.com>
2024-02-02 10:18:34 -0300

    Fix bug in darDeBajaActividad function

--- api/src/controllers/users/darDeBajaActividad.js
+++ api/src/controllers/users/darDeBajaActividad.js
@@ -17,23 +17,24 @@ export const darDeBajaActividad = async (req, res) => {
       await user.save();
 
       //borro al usuario de la actividad
-      const actividadABorrar = await Activity.findById(idActividad);
+      let actividadABorrar = await Activity.findById(idActividad);
       actividadABorrar.users = actividadABorrar.users.filter(
         (usuario) => usuario != id
       );
+      actividadABorrar.cupos = actividadABorrar.cupos + 1;
       await actividadABorrar.save();
 
-      res.status(200).json({
+      return res.status(200).json({
         status: "success",
         message: "Actividad dada de baja con exito",
       });
     } else {
-      res.status(400).json({
+      return res.status(400).json({
         status: "error",
         message: "El usuario no esta inscripto en la actividad",
       });
     }
   } catch (error) {
-    res.status(400).json({ status: "error", message: error.message });
+    return res.status(400).json({ status: "error", message: error.message });
   }
 };
