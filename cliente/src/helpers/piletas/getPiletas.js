1be7eac2ab98cbc43abe979187b1843e7db86683
francogb98 <francobaudino98@gmail.com>
2024-02-05 12:26:00 -0300

    fetching-piletas

--- cliente/src/helpers/piletas/getPiletas.js
+++ cliente/src/helpers/piletas/getPiletas.js
@@ -2,7 +2,7 @@ import { baseUrl } from "../url";
 
 export const getPiletas = async () => {
   try {
-    const url = `${baseUrl}pileta/getPiletas`;
+    const url = `${baseUrl}pileta/getAll`;
     const resp = await fetch(url);
     const data = await resp.json();
     return data;
