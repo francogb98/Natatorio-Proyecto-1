35b2f30effb0806de694297a9692cc737bb161c9
francogb98 <francobaudino98@gmail.com>
2025-01-31 11:35:23 -0300

    asdas

--- cliente/src/ModificandoPagina/Modificando.jsx
+++ cliente/src/ModificandoPagina/Modificando.jsx
@@ -3,13 +3,13 @@ import Logo from "../assets/Logo.png";
 function Modificando() {
   return (
     <div
-      className="container-fluid vh-100 d-flex flex-column align-items-center justify-content-center"
+      className="container-fluid vh-100 w-100 d-flex flex-column align-items-center justify-content-center"
       style={{
         background: "lightblue",
       }}
     >
       <div
-        className="w-100 d-flex justify-content-center"
+        className="d-flex justify-content-center w-100"
         style={{
           position: "absolute",
           top: 0,
@@ -22,12 +22,12 @@ function Modificando() {
           alt="logo"
           style={{
             height: "100px",
-            width: "auto",
+            width: "80%",
           }}
         />
       </div>
-      <div className="text-center">
-        <h1 className="mb-4 text-dark">Estamos Actualizando la página</h1>
+      <div className="text-center mt-5">
+        <h3 className="mb-4 text-dark">Estamos Actualizando la página</h3>
         <div className="mb-4">
           <img
             src="https://media3.giphy.com/media/huOej08UYQYtAjH22E/giphy.gif?cid=ecf05e4722zlimap43nxwgs1uw1zfah471tybh9f7d5c2iap&amp;ep=v1_gifs_related&amp;rid=giphy.gif&amp;ct=s"
